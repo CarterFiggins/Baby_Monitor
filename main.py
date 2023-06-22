655d8525b60269f764703af4d5ca441ee0ff891c
Carter Figgins <Carter.Figgins@atomicjolt.com>
2023-06-22 16:36:39 -0600

    no stopped

--- main.py
+++ main.py
@@ -26,9 +26,6 @@ def index():
 def gen(camera):
     #get camera frame
     while True:
-        if camera.stopped:
-            print("camera stopped")
-            break
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
